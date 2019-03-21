4302b66f7a13da9ad708b4397bcd6b2b8e88a240
Lucas Freund <lcs.freund@gmail.com>
2019-03-21 09:05:44 +0100

    added heading

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
+          <h1>Fib Calculator</h1>
             <img src={logo} className="App-logo" alt="logo" />
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
